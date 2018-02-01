64b67a99776a3267cb06486208a526207886fa73
Oleksii Fedorov <waterlink000@gmail.com>
2018-02-01 08:10:09 +0100

    Write down steps for filling the expense data. Make expense_date_input work together with the date picker and fix the test to do the right thing

--- presentation/src/androidTest/java/com/iwillteachyoukotlin/expensemanager/presentation/expense/EnterExpenseFeatureInstrumentedTest.kt
+++ presentation/src/androidTest/java/com/iwillteachyoukotlin/expensemanager/presentation/expense/EnterExpenseFeatureInstrumentedTest.kt
@@ -1,20 +1,26 @@
 package com.iwillteachyoukotlin.expensemanager.presentation.expense
 
+import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.PickerActions
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.text.InputType.*
+import android.widget.DatePicker
 import com.iwillteachyoukotlin.expensemanager.HomeActivity
 import com.iwillteachyoukotlin.expensemanager.R
+import org.hamcrest.Matchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.text.DateFormat
 import java.text.NumberFormat
 import java.util.*
+import java.util.Calendar.*
 
 
 @RunWith(AndroidJUnit4::class)
@@ -73,8 +79,62 @@ class EnterExpenseFeatureInstrumentedTest {
         onView(withId(R.id.expense_comment_input))
                 .check(matches(withText("")))
 
+        // When I enter all the data:
+        // - Date = yesterday
+        val yesterdayCalendar = Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_MONTH, -1)
+        }
+        val yesterday = dateFormat.format(yesterdayCalendar.time)
+        // tap on the date input once to trigger date picker dialog
+        onView(withId(R.id.expense_date_input)).perform(click())
+        // set date using the date picker dialog
+        onView(withClassName(equalTo(DatePicker::class.java.name)))
+                .perform(PickerActions.setDate(
+                        yesterdayCalendar.get(YEAR),
+                        // de-normalizing (switching to 1-based index)
+                        yesterdayCalendar.get(MONTH) + 1,
+                        yesterdayCalendar.get(DAY_OF_MONTH)
+                ))
+        // confirm selection of that date
+        onView(withId(android.R.id.button1)).perform(click())
+        // verify the correct date was selected
+        onView(withId(R.id.expense_date_input))
+                .check(matches(withText(yesterday)))
 
-        // When I enter all the data
+        // - Amount = 17.30
+        val expectedAmount = 17.30
+        val formattedAmount = decimalFormat.format(expectedAmount)
+        onView(withId(R.id.expense_amount_input))
+                .perform(typeText(formattedAmount))
+                .check(matches(withText(formattedAmount)))
+
+        // - Currency drop down = EUR
+        // 1. open the spinner
+        onView(withId(R.id.expense_currency_spinner))
+                .perform(click())
+        // 2. when data is loaded and shown, click on “EUR”
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("EUR")))
+                .perform(click())
+        // 3. double-check that it is actually selected
+        onView(withId(R.id.expense_currency_spinner))
+                .check(matches(withSpinnerText("EUR")))
+
+        // - Needs reimbursement = true
+        onView(withId(R.id.expense_reimbursement_switch))
+                .perform(click())
+                .check(matches(isChecked()))
+
+        // - Client-related = true
+        onView(withId(R.id.expense_client_related_switch))
+                .perform(click())
+                .check(matches(isChecked()))
+
+        // - Comment = “taxi to the airport”
+        val expectedComment = "taxi to the airport"
+        onView(withId(R.id.expense_comment_input))
+                .perform(typeText(expectedComment))
+                .check(matches(withText(expectedComment)))
+
         // And I tap on “Save” button
         // Then I see the “Expense Details” screen
         // And I see all the data I’ve entered
--- presentation/src/main/java/com/iwillteachyoukotlin/expensemanager/presentation/expense/EnterExpenseActivity.kt
+++ presentation/src/main/java/com/iwillteachyoukotlin/expensemanager/presentation/expense/EnterExpenseActivity.kt
@@ -1,13 +1,16 @@
 package com.iwillteachyoukotlin.expensemanager.presentation.expense
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ArrayAdapter
 import com.iwillteachyoukotlin.expensemanager.R
 import kotlinx.android.synthetic.main.activity_enter_expense.*
 import java.text.DateFormat
 import java.text.NumberFormat
 import java.util.*
+import java.util.Calendar.*
 
 class EnterExpenseActivity : AppCompatActivity() {
 
@@ -31,5 +34,33 @@ class EnterExpenseActivity : AppCompatActivity() {
                 R.array.currency_entries,
                 android.R.layout.simple_spinner_item
         )
+    }
+
+    fun onDateInputClick(view: View) {
+        val today = Calendar.getInstance()
+
+        val dialog = DatePickerDialog(
+                this,
+                { _, year, month, dayOfMonth ->
+                    updateDate(year, month, dayOfMonth)
+                },
+                today.get(YEAR),
+                today.get(MONTH),
+                today.get(DAY_OF_MONTH)
+        )
+
+        dialog.setTitle(R.string.pick_expense_date)
+        dialog.show()
+    }
+
+    private fun updateDate(year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance().apply {
+            set(year, month, dayOfMonth)
+        }
+
+        val dateFormat = DateFormat.getDateInstance()
+        val formattedDate = dateFormat.format(calendar.time)
+
+        expense_date_input.setText(formattedDate)
     }
 }
